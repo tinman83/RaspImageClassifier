584c9bec795735118eff69a0e9a37bc32429c628
Tinashe Manditereza <tmanditereza@gmail.com>
2019-11-24 14:42:44 +0200

    added model files

---
+++ flags.py
@@ -0,0 +1,10 @@
+
+class ProcessFlags(object):
+    def __init__(self):
+        self.predict = False
+
+    def getPredictFlag(self):
+        return self.predict
+        
+    def setPredictFlag(state):
+        self.predict=state
